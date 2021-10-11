e109cc022e74ea36fbd928a47fd56bc121f32bb8
Kai <kaikornegay0356@gmail.com>
2021-10-11 18:06:23 -0500

    Re-wrote play round function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const gameOptions = ["Rock", "Paper", "Scissors"]
+const gameOptions = ["Paper", "Rock", "Scissors"]
 
 let playerChoice = playerPrompt()
+let computerChoice = computerPlay()
 
 let playerWin = false
 let computerWin = false
@@ -13,46 +14,30 @@ function playerPrompt(){
     return playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase()
 }
 
-console.log(playerChoice)
-
 function computerPlay(){
     return gameOptions[Math.floor(Math.random() * gameOptions.length)]
+
 }
 
-console.log(computerPlay())
+// ------------------------------------------------------------------//
 
 function playRound(playerSelection, computerSelection){
+    console.log(`Player choice is ${playerChoice}, computer choice is ${computerChoice}`)
     if (playerSelection === computerSelection){
         playerWin = false
         computerWin = false
+        console.log(`playerWin is ${playerWin} and computerWin is ${computerWin}`)
     } else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" ||playerSelection === "Scissors" && computerSelection === "Paper"){
         playerWin = true
         computerWin = false
+        console.log(`playerWin is ${playerWin} and computerWin is ${computerWin}`)
+        return `You win this round! ${playerSelection} beats ${computerSelection}!`
     } else if (playerSelection === "Scissors" && computerSelection === "Rock" || playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors"){
         playerWin = false
         computerWin = true
+        console.log(`playerWin is ${playerWin} and computerWin is ${computerWin}`)
+        return `You lose this round! ${computerSelection} beats ${playerSelection}!`
     }
-
-    console.log(playerWin)
-    console.log(computerWin)
-
-    if (playerWin === true && computerWin === false){
-        playerScore += 1
-        computerScore = computerScore
-        return `Player score is ${playerScore}. Computer score is ${computerScore}. You win this round! ${playerSelection} beats ${computerSelection}!`
-    } else if (playerWin === false && computerWin === true){
-        playerScore = playerScore
-        computerScore += 1
-        return `Player score is ${playerScore}. Computer score is ${computerScore}. You lose this round! ${computerSelection} beats ${playerSelection}!`
-    } else if (playerWin === false && computerWin === false){
-        return `It's a tie!`
-    }
-
-    //playerPrompt()
-
-    console.log(playerScore)
-    console.log(computerScore)
-    
 }
 
-console.log(playRound(playerChoice, computerPlay()))
+playRound(playerChoice, computerChoice)
