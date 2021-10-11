f9da5c8565ea9ac2c5d2560ab153c95d9659c7d9
Kai <kaikornegay0356@gmail.com>
2021-10-11 17:06:24 -0500

    Added play round function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameOptions = ["Rock", "Papar", "Scissors"]
+const gameOptions = ["Rock", "Paper", "Scissors"]
 
 let playerChoice = playerPrompt()
 
@@ -20,3 +20,39 @@ function computerPlay(){
 }
 
 console.log(computerPlay())
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        playerWin = false
+        computerWin = false
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" ||playerSelection === "Scissors" && computerSelection === "Paper"){
+        playerWin = true
+        computerWin = false
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock" || playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors"){
+        playerWin = false
+        computerWin = true
+    }
+
+    console.log(playerWin)
+    console.log(computerWin)
+
+    if (playerWin === true && computerWin === false){
+        playerScore += 1
+        computerScore = computerScore
+        return `Player score is ${playerScore}. Computer score is ${computerScore}. You win this round! ${playerSelection} beats ${computerSelection}!`
+    } else if (playerWin === false && computerWin === true){
+        playerScore = playerScore
+        computerScore += 1
+        return `Player score is ${playerScore}. Computer score is ${computerScore}. You lose this round! ${computerSelection} beats ${playerSelection}!`
+    } else if (playerWin === false && computerWin === false){
+        return `It's a tie!`
+    }
+
+    //playerPrompt()
+
+    console.log(playerScore)
+    console.log(computerScore)
+    
+}
+
+console.log(playRound(playerChoice, computerPlay()))
